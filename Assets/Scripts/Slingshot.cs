c4b7ee492e1cbbe368dc1b90de5c00b7195e5069
aserk1997 <57926170+aserk1997@users.noreply.github.com>
2020-02-18 22:23:07 -0500

    Set code for halo
    When mouse hovers over halo it glows, otherwise it stays off

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launcher;
+
+    private bool isAiming;
+    private float sphereRadius;
+
+    public GameObject prefabCannonball;
+    public GameObject activeCannonball;
+    public float speedMultiplier = 10.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        launcher.SetActive(false);
+        isAiming = false;
+        sphereRadius = this.GetComponent<SphereCollider>().radius;
+        activeCannonball = null;
+    }
+
+    private void OnMouseEnter()
+    {
+        launcher.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        launcher.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
