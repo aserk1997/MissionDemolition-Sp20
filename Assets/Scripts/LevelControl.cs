bf94777eee0e7efa51263862d9ee43ebf9e035ff
aserk1997 <57926170+aserk1997@users.noreply.github.com>
2020-02-19 01:14:05 -0500

    Tried to connect doorway to new scene but failed

--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LevelControl : MonoBehaviour {  
+public class LevelControl : MonoBehaviour {
 
-    public int index;
     public string levelName;
-
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-            SceneManager.LoadScene(index);
+        if (other.CompareTag("Cannonball"))
             SceneManager.LoadScene(levelName);
     }
 }
