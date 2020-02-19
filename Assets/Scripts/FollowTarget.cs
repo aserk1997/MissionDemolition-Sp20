883e62c2a1d8cc1b433758e6f322053a2c22a400
aserk1997 <57926170+aserk1997@users.noreply.github.com>
2020-02-18 23:05:34 -0500

    Script to follow the cannonball
    Set up objects

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public GameObject targetObject;
+    private Vector3 initialPosition;
+
+    public GameObject ground;
+    private Vector3 groundPosition;
+
+    public float easing = 0.05f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
